e0fd83e6b45f7c2953a59f1eb8d4230f3a6b28b1
amir <shapkotaamir0@gmail.com>
2025-10-30 11:18:19 +0545

    changed allowed hosts

--- journal_portal/settings.py
+++ journal_portal/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-*_g=2g#r^7$7h$1qxy1iu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
